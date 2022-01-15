65944eeea92f058d1e8b6a5dfcad0b16aaef5eaa
Logan Woolf <loganwoolf@gmail.com>
2022-01-15 05:36:05 +0000

    Completed sum.js using recursion

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // base case
+  if (fromN === toN) {
+    return fromN
+  }
+
+  // recursive case checking number order
+  // add
+  return fromN < toN
+    ? fromN + sum(fromN + 1, toN)
+    : fromN + sum(fromN - 1, toN)
 }
 
 module.exports = sum;
